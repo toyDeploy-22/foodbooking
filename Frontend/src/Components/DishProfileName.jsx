ff95dcd2879fc3b054cfd365b570fee589e41325
Eelismey-jf <eelismey@gmail.com>
2025-03-19 22:08:47 +0100

    rectifier

--- Frontend/src/Components/DishProfileName.jsx
+++ Frontend/src/Components/DishProfileName.jsx
@@ -45,8 +45,8 @@ if(Error === true) {
     return(ErrorElement)
 } else {
 return (
-        <Stack>
-<h3 className="bg-success p-2 text-light">{dishSelected.dish_name ? dishSelected.dish_name : "Dish Card"}</h3>
+        <Stack className="text-center">
+<h3 className="bg-success border border-dark p-2 text-light rounded-start-pill">{dishSelected.dish_name ? dishSelected.dish_name : "Dish Card"}</h3>
             { loader === true &&
                 <LoadingElement />
             }
@@ -63,12 +63,12 @@ return (
                  onError={() => setNoLink(true)}
                />
                <Figure.Caption>
-               <p className="bg-secondary text-light fst-italic">{dishSelected.dish_description}</p>
+               <p className="bg-secondary text-light fst-italic w-75 mx-auto">{dishSelected.dish_description}</p>
                <ul className="ingredients" >
                 <li 
                 style={{listStyle: 'none', fontFamily: 'inherit', fontSize: 'initial'}}
                 className="p-2 border border-primary text-primary fw-bold">Ingredients:</li>
-                <br /><div className="border border-dark dishMenuColor" >
+                <br /><div className="border rounded-3 border-dark dishMenuColor p-5" >
                 {
                     dishSelected.dish_ingredients.map((igr, _ind)=><li 
                     className="text-light pt-1 pb-3"
@@ -77,7 +77,7 @@ return (
                 </div>
                </ul>
                <br />
-               <h5 className="bg-dark text-light p-2">{dishSelected.dish_price.toFixed(2)}{dishSelected.dish_symbol}</h5>
+               <h5 className="bg-dark text-light p-2 w-75 mx-auto rounded-end-pill">{dishSelected.dish_price.toFixed(2)}{dishSelected.dish_symbol}</h5>
                </Figure.Caption>
              </Figure>
              :
