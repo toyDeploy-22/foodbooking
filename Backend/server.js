f231568e27a2d983f61805b09cac101c0169a920
Eelismey-jf <eelismey@gmail.com>
2025-03-22 00:31:43 +0100

    rectifier

--- Backend/server.js
+++ Backend/server.js
@@ -52,7 +52,7 @@ myServer.get("/", (req, res) => {
 		res.setHeader('db-Host', details.host);
 		res.setHeader('db-Name', details.database)
 		})*/
-		.then((data) => res.json(data.connection))
+		.then((data) => res.json(data))
 		//.then(() => res.sendFile(htmlSuccessPage))
 		.catch((err) => { 
 			console.error(err);
